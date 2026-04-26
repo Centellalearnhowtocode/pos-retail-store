6a9c3ac11c59999c687b6af852f3cca12c96815e
centellalearnhowtocode <chornay126@gmail.com>
2026-04-27 00:20:00 +0700

    commit to main

--- Coding Space/Library/dashboard.c
+++ Coding Space/Library/dashboard.c
@@ -1,5 +1,22 @@
 #include <gtk/gtk.h>
-    GtkWidget* create_numpad() {
+#include <stdio.h>
+
+// 1. Define load_css at the top so main can see it
+void load_css(void) {
+    GtkCssProvider *provider = gtk_css_provider_new();
+    GdkDisplay *display = gdk_display_get_default();
+    GdkScreen *screen = gdk_display_get_default_screen(display);
+
+    gtk_style_context_add_provider_for_screen(screen, 
+        GTK_STYLE_PROVIDER(provider), 
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    // Make sure style.css is in the same folder!
+    gtk_css_provider_load_from_path(provider, "style.css", NULL);
+    g_object_unref(provider);
+}
+
+GtkWidget* create_numpad() {
     GtkWidget *grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
@@ -13,7 +30,14 @@
 
     for (int i = 0; i < 20; i++) {
         GtkWidget *btn = gtk_button_new_with_label(labels[i]);
-        gtk_widget_set_name(btn, (g_strcmp0(labels[i], "Void") == 0) ? "btn-void" : (g_strcmp0(labels[i], "Submit") == 0) ? "btn-submit" : "btn-pos");
+        // Set CSS names for colors
+        if (g_strcmp0(labels[i], "Void") == 0) {
+            gtk_widget_set_name(btn, "btn-void");
+        } else if (g_strcmp0(labels[i], "Submit") == 0) {
+            gtk_widget_set_name(btn, "btn-submit");
+        } else {
+            gtk_widget_set_name(btn, "btn-pos");
+        }
         gtk_grid_attach(GTK_GRID(grid), btn, i % 5, i / 5, 1, 1);
     }
     return grid;
@@ -21,10 +45,13 @@
 
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
+    
+    // 2. Load CSS before creating the window
     load_css();
 
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(window), 1024, 600);
+    gtk_window_set_title(GTK_WINDOW(window), "POS Dashboard");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     GtkWidget *main_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
@@ -33,7 +60,7 @@ int main(int argc, char *argv[]) {
     // Header
     GtkWidget *header = gtk_label_new("Date: 2026-04-26 | Shift: Morning | Bill #: 0000");
     gtk_widget_set_name(header, "header-bar");
-    gtk_box_pack_start(GTK_BOX(main_vbox), header, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(main_vbox), header, FALSE, FALSE, 10);
 
     GtkWidget *content_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     gtk_box_pack_start(GTK_BOX(main_vbox), content_hbox, TRUE, TRUE, 0);
@@ -55,9 +82,9 @@ int main(int argc, char *argv[]) {
     GtkWidget *right_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_widget_set_name(right_vbox, "right-panel");
     
-    GtkWidget *order_list = gtk_label_new("Item Name 1   1 x $5.00   $5.00\nItem Name 2   2 x $5.00   $10.00");
+    GtkWidget *order_list = gtk_label_new("Item Name 1    1 x $5.00    $5.00\nItem Name 2    2 x $5.00    $10.00");
     gtk_widget_set_name(order_list, "order-area");
-    gtk_box_pack_start(GTK_BOX(right_vbox), order_list, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(right_vbox), order_list, TRUE, TRUE, 10);
 
     gtk_box_pack_start(GTK_BOX(right_vbox), create_numpad(), FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(content_hbox), right_vbox, FALSE, FALSE, 0);
@@ -65,20 +92,5 @@ int main(int argc, char *argv[]) {
     gtk_widget_show_all(window);
     gtk_main();
 
-     /// CSS
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(provider, "style.css", NULL);
-
-    gtk_style_context_add_provider_for_display(
-        gdk_display_get_default(),
-        GTK_STYLE_PROVIDER(provider),
-        GTK_STYLE_PROVIDER_PRIORITY_USER
-    );
-
-    g_object_unref(provider);
-
-    // Show window
-    gtk_widget_show_all(window);
     return 0;
-
 }
